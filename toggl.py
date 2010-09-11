d8fa2fa7db390282489a810b24a43956b8becd89
Marc Dougherty <muncus@gmail.com>
2010-09-11 09:52:43 -0700

    request auth to toggl now works.

--- toggl.py
+++ toggl.py
@@ -6,8 +6,13 @@
 
 from optparse import OptionParser
 from sys import stdin
+import base64
 import urllib2
 import yaml
+import json
+
+#TODO: remove the need for a yaml map of rfid->task
+#TODO: turn the yaml map into a json map, so i dont need both formats.
 
 parser = OptionParser()
 parser.add_option("-k", "--toggl_key", dest='apikey', help="Toggl api key")
@@ -20,9 +25,17 @@ TAG_MAP = yaml.load(file(options.mapfile))
 TASKS = []
 print TAG_MAP
 
+def addAuthHeader(request, apikey):
+    """ This is needed because the toggl servers dont do standard http basic auth.
+    """
+    base64string = base64.encodestring('%s:%s' % (apikey, 'api_token')).replace('\n', '')
+    request.add_header("Authorization", "Basic %s" % base64string)   
+    return request
+
 def getTasks():
     r = urllib2.Request("http://www.toggl.com/api/v3/tasks.json")
-    #TODO: do the auth dance.
+    r = addAuthHeader(r, options.apikey)
+
     f = urllib2.urlopen(r)
     TASKS = (json.load(f))['data']
     print TASKS
@@ -34,13 +47,14 @@ def updateToggl(task_name, duration):
     # map needs to map to a task id.
     # when we get input, fetch the task associated with the tag, increment the duration, and post it.
 
-while True:
-    s = stdin.readline()
-    if not s:
-        break
-    (key, duration) = s.split() 
-    print "key: %s" % key
-    if(TAG_MAP.has_key(key)):
-        print TAG_MAP[key]
-    else:
-        print "unknown tag: %s" % key
+getTasks()
+#while True:
+#    s = stdin.readline()
+#    if not s:
+#        break
+#    (key, duration) = s.split() 
+#    print "key: %s" % key
+#    if(TAG_MAP.has_key(key)):
+#        print TAG_MAP[key]
+#    else:
+#        print "unknown tag: %s" % key
