f62a8fbf1a6e7c036fdf2b4c1608ad2d1a21829b
Marc Dougherty <muncus@gmail.com>
2010-09-11 00:40:04 -0700

    a bit more work on this, little progress so far

--- toggl.py
+++ toggl.py
@@ -16,20 +16,30 @@ parser.add_option("-m", "--tagmap", dest='mapfile',
 (options, args) = parser.parse_args()
 
 # dictionary of tag number -> string
-TAG_MAP = yaml.load(options.mapfile)
+TAG_MAP = yaml.load(file(options.mapfile))
+TASKS = []
+print TAG_MAP
+
+def getTasks():
+    r = urllib2.Request("http://www.toggl.com/api/v3/tasks.json")
+    #TODO: do the auth dance.
+    f = urllib2.urlopen(r)
+    TASKS = (json.load(f))['data']
+    print TASKS
 
 def updateToggl(task_name, duration):
     """update the supplied task on toggl.com.
     """
-    req = urllib2.Request("http://www.toggl.com/api/v3/tasks/5344561.json")
-    # crud. the post url changes, depending on the task. cant just say "start
-    # this" or "start that"
+    pass
+    # map needs to map to a task id.
+    # when we get input, fetch the task associated with the tag, increment the duration, and post it.
 
 while True:
     s = stdin.readline()
     if not s:
         break
-    (key, duration) = s.split(maxsplit=1) 
+    (key, duration) = s.split() 
+    print "key: %s" % key
     if(TAG_MAP.has_key(key)):
         print TAG_MAP[key]
     else:
